f3fe012e676dcc59e2b47a65df9a11164a0cf43b
KatherineRodgeCC <katherine.rodge@corporateconversions.com>
2023-04-05 10:58:50 -0400

    Update locations.js

--- pages/locations.js
+++ pages/locations.js
@@ -1,91 +1,48 @@
-import * as MENUS from 'constants/menus';
+import { gql } from "@apollo/client";
 
-import { gql } from '@apollo/client';
-import {
-  Header,
-  EntryHeader,
-  Footer,
-  ProjectHeader,
-  ContentWrapper,
-  NavigationMenu,
-  FeaturedImage,
-  Main,
-  SEO,
-} from 'components';
-import { BlogInfoFragment } from 'fragments/GeneralSettings';
-
-export default function Component(props) {
-  // Loading state for previews
-  if (props.loading) {
-    return <>Loading...</>;
-  }
-  const { title: siteTitle } = props?.data?.generalSettings;
-  const primaryMenu = props?.data?.headerMenuItems?.nodes ?? [];
-  const footerMenu = props?.data?.footerMenuItems?.nodes ?? [];
-  const { title, summary, featuredImage, contentArea } = props.data.project;
-  return (
-    <>
-      <SEO
-        title={`${title} - ${props?.data?.generalSettings?.title}`}
-        imageUrl={featuredImage?.node?.sourceUrl}
-      />
-
-      <Header menuItems={primaryMenu} />
-
-      <Main>
-        <EntryHeader title={title} />
-        <ProjectHeader
-          image={featuredImage?.node}
-          summary={summary}
-          title={title}
-        />
-        <div className="container">
-          <ContentWrapper content={contentArea} />
-        </div>
-      </Main>
-
-      <Footer title={siteTitle} menuItems={footerMenu} />
-    </>
-  );
-}
-
-Component.query = gql`
-  ${BlogInfoFragment}
-  ${NavigationMenu.fragments.entry}
-  ${FeaturedImage.fragments.entry}
-  query GetPost(
-    $databaseId: ID!
-    $headerLocation: MenuLocationEnum
-    $footerLocation: MenuLocationEnum
-    $asPreview: Boolean = false
-  ) {
-    project(id: $databaseId, idType: DATABASE_ID, asPreview: $asPreview) {
-      title: projectTitle
-      summary
-      contentArea
-      ...FeaturedImageFragment
-    }
-    generalSettings {
-      ...BlogInfoFragment
-    }
-    headerMenuItems: menuItems(where: { location: $headerLocation }) {
+const GET_PROJECTS = gql`
+  query getLocations {
+    locations(first: 10, after: null) {
       nodes {
-        ...NavigationMenuItemFragment
-      }
-    }
-    footerMenuItems: menuItems(where: { location: $footerLocation }) {
-      nodes {
-        ...NavigationMenuItemFragment
+        facilityName
+        address
+        content
       }
     }
   }
 `;
 
-Component.variables = ({ databaseId }, ctx) => {
+export default function Locations(props) {
+  const { locations } = props;
+
+  return (
+    <div>
+      <h1>Locations</h1>
+      <ul className="locations-list">
+        {locations.map((location) => (
+          <li key={location.databaseId}>
+            <article className="project-card">
+              <h2>
+                <Link href={`/locations/${location.databaseId}`}>
+                  <a>{location.facilityName}</a>
+                </Link>
+              </h2>
+            </article>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export async function getStaticProps() {
+  const response = await client.query({
+    query: GET_PROJECTS,
+  });
+
   return {
-    databaseId,
-    headerLocation: MENUS.PRIMARY_LOCATION,
-    footerLocation: MENUS.FOOTER_LOCATION,
-    asPreview: ctx?.asPreview,
+    props: {
+      projects: response.data.projects.nodes,
+    },
   };
-};
+}
