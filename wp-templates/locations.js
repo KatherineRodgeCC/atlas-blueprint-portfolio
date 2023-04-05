1f2e77e494ef0f8627ea53b7116430714ea1a48b
KatherineRodgeCC <katherine.rodge@corporateconversions.com>
2023-04-05 11:56:29 -0400

    Update locations.js

--- wp-templates/locations.js
+++ wp-templates/locations.js
@@ -1,48 +1,163 @@
-import { gql } from "@apollo/client";
+import * as MENUS from 'constants/menus';
 
-const GET_PROJECTS = gql`
-  query getLocations {
-    locations(first: 10, after: null) {
+import { useQuery, gql } from '@apollo/client';
+import { FaArrowRight } from 'react-icons/fa';
+import styles from 'styles/pages/_Home.module.scss';
+import {
+  EntryHeader,
+  Main,
+  Button,
+  Heading,
+  CTA,
+  NavigationMenu,
+  SEO,
+  Header,
+  Footer,
+  Posts,
+  Testimonials,
+} from 'components';
+import { BlogInfoFragment } from 'fragments/GeneralSettings';
+
+const postsPerPage = 3;
+
+export default function Component() {
+  const { data, loading } = useQuery(Component.query, {
+    variables: Component.variables(),
+  });
+  if (loading) {
+    return null;
+  }
+
+  const { title: siteTitle, description: siteDescription } =
+    data?.generalSettings;
+  const primaryMenu = data?.headerMenuItems?.nodes ?? [];
+  const footerMenu = data?.footerMenuItems?.nodes ?? [];
+
+  const mainBanner = {
+    sourceUrl: '/static/banner.jpeg',
+    mediaDetails: { width: 1200, height: 600 },
+    altText: 'Portfolio Banner',
+  };
+  return (
+    <>
+      <SEO title={siteTitle} description={siteDescription} />
+
+      <Header
+        title={siteTitle}
+        description={siteDescription}
+        menuItems={primaryMenu}
+      />
+
+      <Main className={styles.home}>
+        <EntryHeader image={mainBanner} />
+        <div className="container">
+          <section className="hero text-center">
+            <Heading className={styles.heading} level="h1">
+              Welcome to your Blueprint
+            </Heading>
+            <p className={styles.description}>
+              Achieve unprecedented performance with modern frameworks and the
+              world&apos;s #1 open source CMS in one powerful headless platform.{' '}
+            </p>
+            <div className={styles.actions}>
+              <Button styleType="secondary" href="/contact-us">
+                GET STARTED
+              </Button>
+              <Button styleType="primary" href="/about">
+                LEARN MORE
+              </Button>
+            </div>
+          </section>
+          <section className="cta">
+            <CTA
+              Button={() => (
+                <Button href="/posts">
+                  Get Started <FaArrowRight style={{ marginLeft: `1rem` }} />
+                </Button>
+              )}
+            >
+              <span>
+                Learn about Core Web Vitals and how Atlas can help you reach
+                your most demanding speed and user experience requirements.
+              </span>
+            </CTA>
+          </section>
+          <section className={styles.posts}>
+            <Heading className={styles.heading} level="h2">
+              Latest Posts
+            </Heading>
+            <Posts posts={data.posts?.nodes} id="posts-list" />
+          </section>
+          <section className="cta">
+            <CTA
+              Button={() => (
+                <Button href="/posts">
+                  Get Started <FaArrowRight style={{ marginLeft: `1rem` }} />
+                </Button>
+              )}
+            >
+              <span>
+                Learn about Core Web Vitals and how Atlas can help you reach
+                your most demanding speed and user experience requirements.
+              </span>
+            </CTA>
+          </section>
+          <section className={styles.testimonials}>
+            <Heading className={styles.heading} level="h2">
+              Testimonials
+            </Heading>
+            <p className={styles.description}>
+              Here are just a few of the nice things our customers have to say.
+            </p>
+            <Testimonials testimonials={data?.testimonials?.nodes} />
+          </section>
+        </div>
+      </Main>
+      <Footer menuItems={footerMenu} />
+    </>
+  );
+}
+
+Component.variables = () => {
+  return {
+    headerLocation: MENUS.PRIMARY_LOCATION,
+    footerLocation: MENUS.FOOTER_LOCATION,
+    first: postsPerPage,
+  };
+};
+
+Component.query = gql`
+  ${BlogInfoFragment}
+  ${NavigationMenu.fragments.entry}
+  ${Posts.fragments.entry}
+  ${Testimonials.fragments.entry}
+  query GetPageData(
+    $headerLocation: MenuLocationEnum
+    $footerLocation: MenuLocationEnum
+    $first: Int
+  ) {
+    posts(first: $first) {
       nodes {
-        facilityName
-        address
-        content
+        ...PostsItemFragment
+      }
+    }
+    testimonials {
+      nodes {
+        ...TestimonialsFragment
+      }
+    }
+    generalSettings {
+      ...BlogInfoFragment
+    }
+    headerMenuItems: menuItems(where: { location: $headerLocation }) {
+      nodes {
+        ...NavigationMenuItemFragment
+      }
+    }
+    footerMenuItems: menuItems(where: { location: $footerLocation }) {
+      nodes {
+        ...NavigationMenuItemFragment
       }
     }
   }
 `;
-
-export default function Locations(props) {
-  const { locations } = props;
-
-  return (
-    <div>
-      <h1>Locations</h1>
-      <ul className="locations-list">
-        {locations.map((location) => (
-          <li key={location.databaseId}>
-            <article className="project-card">
-              <h2>
-                <Link href={`/locations/${location.databaseId}`}>
-                  <a>{location.facilityName}</a>
-                </Link>
-              </h2>
-            </article>
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
-}
-
-export async function getStaticProps() {
-  const response = await client.query({
-    query: GET_PROJECTS,
-  });
-
-  return {
-    props: {
-      projects: response.data.projects.nodes,
-    },
-  };
-}
