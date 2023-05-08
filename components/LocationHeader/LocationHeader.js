8dc7a63f1ff99bf18b5d747551fd46eea5289c0e
KatherineRodgeCC <katherine.rodge@corporateconversions.com>
2023-05-08 13:27:41 -0400

    Update LocationHeader.js

--- components/LocationHeader/LocationHeader.js
+++ components/LocationHeader/LocationHeader.js
@@ -19,7 +19,7 @@ export default function LocationHeader({ title, image, date, author, className, 
   return (
     <div className={cx(['entry-header', className])}
     style={{
-      backgroundImage: `url(${FeaturedImage?.sourceURL})`,
+      backgroundImage: `url(${image.src})`,
       width: '100%',
       height: '100%',
     }}>
