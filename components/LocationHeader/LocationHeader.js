ddf757e7abd54d4d7395c70203437234551e3da9
KatherineRodgeCC <katherine.rodge@corporateconversions.com>
2023-05-08 11:51:23 -0400

    Update LocationHeader.js

--- components/LocationHeader/LocationHeader.js
+++ components/LocationHeader/LocationHeader.js
@@ -1,21 +1,7 @@
-import className from 'classnames/bind';
-import { FeaturedImage, Heading } from 'components';
-
-import styles from './LocationHeader.module.scss';
-const cx = className.bind(styles);
-/**
- * A Page or Post entry header component
- * @param {Props} props The props object.
- * @param {string} props.title The post/page title.
- * @param {MediaItem} props.image The image node.
- * @param {string} props.date The post/page publish date.
- * @param {string} props.author The post/page author's name.
- * @param {string} props.className An optional className to be added to the EntryHeader.
  * @return {React.ReactElement} The EntryHeader component.
  */
 export default function LocationHeader({ title, image, date, author, className, summary }) {
   const hasText = title || date || author;
-
   return (
     <div className={cx(['entry-header', className])}
     style={{
@@ -27,6 +13,18 @@ export default function LocationHeader({ title, image, date, author, className, 
         <div className={cx('text')}>
           {!!title && <Heading className={cx('title')}>{title}</Heading>}
           <p>{summary}</p>
+        </div>
+      )}
+      {image && (
+        <div className={cx('image')}>
+          <div className="container">
+            <FeaturedImage
+              className='LocationImage'
+              className='LocationImageTest'
+              image={image}
+              priority
+            />
+          </div>
         </div>
       )}
     </div>
