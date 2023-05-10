50cb4cd22c9dbbea460123d0e4c3829e5c1b6df3
KatherineRodgeCC <katherine.rodge@corporateconversions.com>
2023-05-10 14:37:23 -0400

    Update location.js

--- wp-templates/location.js
+++ wp-templates/location.js
@@ -83,12 +83,6 @@ export default function Component(props) {
              </div>
            </div>
           </div>
-          <div className="row">
-            <video width="320" height="240" controls>
-              <source src={`${videolink}`} type="video/mp4">
-              </source>   
-            </video>
-          </div>
         </div>
       </Main>
 
