e3df1ca180b21fb36b0e8f90d1724a8b98db1561
KatherineRodgeCC <katherine.rodge@corporateconversions.com>
2023-05-11 14:00:38 -0400

    Update LocationMainInformation.js

--- components/LocationMainInformation/LocationMainInformation.js
+++ components/LocationMainInformation/LocationMainInformation.js
@@ -1,33 +1,32 @@
 import React from 'react';
-import { Heading } from 'components';
+import { Heading, ContentWrapper } from 'components';
 import className from 'classnames/bind';
 
 import styles from './LocationMainInformation.module.scss';
 const cx = className.bind(styles);
 /**
  * A Project entry header component
- * @param {string} props.titleThe project content.
  * @param {string} props.content The project content.
  * @param {string} props.address The project address.
  * @returns {React.ReactElement} The PostInfo component
  */
-function LocationMainInformation({ title, content, address }) {
+function LocationMainInformation({ content, address }) {
   return (
     <section className='content-info'>
         <div className="row row-center">
           <div className={cx('column')}> 
-              <Heading level="h3">{title}</Heading>
+              <Heading level="h3">Location</Heading>
               <p>{address}</p>
               <p><a href="#">Map Link</a></p>
-              <Heading level="h3">{title}</Heading>
+              <Heading level="h3">Facility</Heading>
               <p>Year:</p>
               <p>Size:</p>
               <p>Union:</p>
               <p>Social:</p>
              </div>
            <div className={cx('column')}>
-              <Heading level="h3">{title}</Heading>
-              <p>{content}</p>
+              <Heading level="h3">What We Do</Heading>
+              <ContentWrapper content={content} />
           </div>
           </div>
     </section>
